11e7a52e8aa8ede910465a40b1191bca6e20f29e
Baioretto <helloworldfirstfta@gmail.com>
2023-12-22 13:43:43 +0200

    Core logic

---
+++ lib/gegnerator.py
@@ -0,0 +1,21 @@
+
+import random
+import string
+
+
+def generate_password(master_key: str, service_name: str, n: int, special_symbols: bool, iterations: int) -> str:
+    random.seed(master_key+str(iterations)+service_name)
+
+    chars = string.ascii_letters + (string.punctuation if special_symbols else "")
+
+    password = ""
+
+    for _ in range(n):
+        password += random.choice(chars)
+
+    return password
+
+
+def ISaver:
+    def save(self):
+        pass
