ab1c293226b80a5a7721f38b17968f355c7aa7ce
PurpleFta <helloworldfirstfta@gmail.com>
2024-01-21 19:28:01 +0200

    Adding numbers to symbols for generation

--- lib/generator.py
+++ lib/generator.py
@@ -6,7 +6,7 @@ import string
 def generate_password(master_key: str, service_name: str, n: int, special_symbols: bool, iterations: int) -> str:
     random.seed(master_key+str(iterations)+service_name)
 
-    chars = string.ascii_letters + (string.punctuation if special_symbols else "")
+    chars = string.ascii_letters + (string.punctuation if special_symbols else "") + "0123456789"
 
     password = ""
 
